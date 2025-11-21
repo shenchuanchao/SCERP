5a65651c8886e6fdf0e2b38f5d1475e8c1903f1e
Vinnie-shen <wxscc@foxmail.com>
2025-11-21 08:56:23 +0800

    优化

--- 前端/ScerpApp/src/router/index.ts
+++ 前端/ScerpApp/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Login from '../components/Login.vue'
+import Login from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,7 +11,7 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/',
+      path: '/login',
       name: 'login',
       component: Login,
     },
